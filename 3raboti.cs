8bb81b6ea9ffe6b6fafa54fc2375614d48f2ea1f
ReKKeKpro <114856261+ReKKeKpro@users.noreply.github.com>
2022-10-06 21:37:01 +0500

    Add files via upload

---
+++ 3raboti.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double first, second, third;
+            Console.WriteLine("Первое число");
+            first = double.Parse(Console.ReadLine());
+            Console.WriteLine("Второе число");
+            second = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите 3 число");
+            third = double.Parse(Console.ReadLine());
+            double sum = first + second + third;
+            double mult = first * second * third;
+            Console.WriteLine("Сумма 3 чисел: " + sum);
+            Console.WriteLine("Произведение 3 чисел: " + mult);
+
+        }
+    }
+}
